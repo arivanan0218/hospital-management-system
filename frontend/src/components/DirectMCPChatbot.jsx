a1b58ac73f91295188ebcf1b0b9b038f383e304f
arivanan0218 <arivanan_v_e23@engug.ruh.ac.lk>
2025-08-11 06:43:17 +0530

    Make change in frontend

--- frontend/src/components/DirectMCPChatbot.jsx
+++ frontend/src/components/DirectMCPChatbot.jsx
@@ -810,7 +810,7 @@ const DirectMCPChatbot = () => {
                     <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
-                    <span>Test Server Connection</span>
+                    {/* <span>Test Server Connection</span> */}
                   </div>
                 </button>
                 
