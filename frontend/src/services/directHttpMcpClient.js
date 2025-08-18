93cfb27d4f0ce32bfbf6a11463ef5b55eecd1296
arivanan0218 <arivanan_v_e23@engug.ruh.ac.lk>
2025-08-18 10:13:51 +0530

    Configure AWS deployment with new EC2 instance

--- frontend/src/services/directHttpMcpClient.js
+++ frontend/src/services/directHttpMcpClient.js
@@ -7,8 +7,8 @@ class DirectHttpMCPClient {
   constructor() {
    // Use relative URL to go through nginx proxy
     // This will use the current domain/port and route through nginx
-    this.serverUrl = '';
-    // this.serverUrl = 'http://localhost:8000';
+    // this.serverUrl = '';
+    this.serverUrl = 'http://localhost:8000';
     this.isConnected = false;
     this.serverInfo = {};
     this.tools = [];
